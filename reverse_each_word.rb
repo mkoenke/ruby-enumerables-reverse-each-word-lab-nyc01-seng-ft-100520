e4285b0726f50aca035503f88c427355242bcb64
Mary Koenke <mkoenke@sas.upenn.edu>
2020-09-17 16:00:21 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -7,3 +7,11 @@ def reverse_each_word(string)
   end
   new_array.join(" ")
 end
+
+def reverse_each_word(string)
+  array = string.split(" ")
+  array.collect do |string1|
+    string1.reverse
+  end
+  array.join(" ")
+end
